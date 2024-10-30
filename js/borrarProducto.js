1bcc55b00025df663c7e8ce7433b34f28db78fc5
BryantGG77 <brayespro@gmail.com>
2024-10-30 13:19:27 -0500

    Trabajando con json-server y agregando solicitudes para listar, agregar o borrar productos 2

--- js/borrarProducto.js
+++ js/borrarProducto.js
@@ -2,6 +2,7 @@ import { conexionAPI } from "./conexionAPI.js";
 
 export function eventoBorrarProducto(producto) {
     producto.querySelector(".productos__boton").addEventListener("click", async (evento) => {
+        evento.preventDefault();
         const id = evento.target.closest("button").dataset.id;
         await conexionAPI.eliminarProducto(id);
         producto.remove(); // Elimina el elemento de la lista en el DOM
--- js/crearProducto.js
+++ js/crearProducto.js
@@ -3,6 +3,8 @@ import { conexionAPI } from "./conexionAPI.js";
 const formulario = document.querySelector("[data-formulario]");
 
 async function crearProducto(evento) {
+    // Prevenir el comportamiento por defecto de enviar el formulario
+    evento.preventDefault();
 
     // Primero obtenemos la lista actual de productos
     const productosExistentes = await fetch('http://localhost:3001/productos');
@@ -11,7 +13,6 @@ async function crearProducto(evento) {
     // Luego calculamos el nuevo ID basándonos en el número de productos actuales
     const id = productos.length + 1;
 
-    evento.preventDefault();
     const nombre = document.querySelector("[data-nombre]").value;
     const precio = parseFloat(document.querySelector("[data-precio]").value);
     const imagen = document.querySelector("[data-imagen]").value;
@@ -20,4 +21,8 @@ async function crearProducto(evento) {
     await conexionAPI.agregarProducto(id, nombre, precio, imagen);
 }
 
-formulario.addEventListener("submit", evento => crearProducto(evento))
+formulario.addEventListener("submit", (evento) => {
+    evento.preventDefault(); // Asegúrate de que esto esté aquí
+    crearProducto(evento);
+});
+
